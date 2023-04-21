e15a23bce31e85823f22e5190b79d8c4dffa6e03
JinseokMo <m747op@gmail.com>
2023-04-21 14:41:41 +0900

    WebNoticeBoard New

---
+++ src/main/java/com/example/webnoticeboard/WebNoticeBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.webnoticeboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebNoticeBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebNoticeBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webnoticeboard/WebNoticeBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webnoticeboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebNoticeBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
